38f68f777a8ed0b5c9a80b62c32ff34949f05d8a
uwudamith <damith.uwu@gmail.com>
2016-05-03 12:28:08 +0530

    commiting changes

--- mytiles/mytiles/src/main/java/com/my/tiles/controllers/MainController.java
+++ mytiles/mytiles/src/main/java/com/my/tiles/controllers/MainController.java
@@ -12,6 +12,6 @@ public class MainController {
 	      model.addAttribute("title", "Login");
 	      model.addAttribute("message", "Enter your username/password:");
 	      return "loginPage";
-	  }
+	  }  
 	  
 }
