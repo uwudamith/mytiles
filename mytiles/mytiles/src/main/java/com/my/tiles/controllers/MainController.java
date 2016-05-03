67491be7e218ff758cccd03f6c12dd8c773e3d3f
uwudamith <damith.uwu@gmail.com>
2016-05-03 11:55:56 +0530

    remove space

--- mytiles/mytiles/src/main/java/com/my/tiles/controllers/MainController.java
+++ mytiles/mytiles/src/main/java/com/my/tiles/controllers/MainController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MainController {
-
 	@RequestMapping(value = { "/", "/login" }, method = RequestMethod.GET)
 	  public String loginPage(Model model) {
 	      model.addAttribute("title", "Login");
