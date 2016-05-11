911f8ed3f710171fafa3cf3f218229ec3efea698
uwudamith <damith.uwu@gmail.com>
2016-05-11 15:11:59 +0530

    Removed mytiles folder

--- mytiles/mytiles/src/main/java/com/my/tiles/repository/EventRepository.java
+++
@@ -1,9 +0,0 @@
-package com.my.tiles.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.my.tiles.entity.Event;
-
-public interface EventRepository extends JpaRepository<Event, Integer>{
-
-}
--- mytiles/mytiles/src/main/webapp/WEB-INF/tags/header/header.tag
+++
@@ -1,4 +0,0 @@
-<%@ taglib prefix="lib" tagdir="/WEB-INF/tags/libs"%>
-<%@ taglib prefix="master" tagdir="/WEB-INF/tags/master"%>
-<lib:css />
-<master:root/>
